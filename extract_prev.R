ef0fc5ce703d45d8137c4d99cd3ca60679fe68de
Rdgomes97 <107285333+Rdgomes97@users.noreply.github.com>
2023-06-27 23:49:48 -0300

    Delete extract_prev.R

--- extract_prev.R
+++
@@ -1,8 +0,0 @@
-# extract_var.R
-
-source("temp_prev_vf2.r") # caminho para o seu arquivo .r
-
-args <- commandArgs(trailingOnly = TRUE)
-var_name <- args[1]
-
-cat(get(var_name)) # imprimir o valor da variável
